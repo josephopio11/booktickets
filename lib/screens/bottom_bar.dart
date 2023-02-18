64dde9d9b7dacf31446f90b9765b849f7986d1e2
Joseph Opio <josephopio11@gmail.com>
2023-02-18 14:30:25 +0800

    Taken a break

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,6 +1,8 @@
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 
+import 'home_screen.dart';
+
 class BottomBar extends StatefulWidget {
   const BottomBar({super.key});
 
@@ -9,9 +11,9 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
-  int _selectedIndex = 1;
+  int _selectedIndex = 0;
   static final List<Widget> _widgetOptions = <Widget>[
-    const Text('Home'),
+    const HomeScreen(),
     const Text('Search'),
     const Text('Tickets'),
     const Text('Profile'),
@@ -26,9 +28,6 @@ class _BottomBarState extends State<BottomBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Tickets'),
-      ),
       body: Center(
         child: _widgetOptions[_selectedIndex],
       ),
